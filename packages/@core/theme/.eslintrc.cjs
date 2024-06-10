fa8acc1cae0562bcd2641f0bff4ab085dd32f8e0
r3dm4st3r <prafullaondemand@gmail.com>
2024-06-11 00:15:01 +0530

    Updated eslint

--- packages/@core/theme/.eslintrc.cjs
+++ packages/@core/theme/.eslintrc.cjs
@@ -1,4 +1,7 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   extends: ["@xtineui/config-eslint/library.js"].map(require.resolve),
+  rules: {
+    "import/no-default-export": "off"
+  }
 };
