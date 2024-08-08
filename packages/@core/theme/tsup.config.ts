dcbcd70df20ed3a02243e6ce98737aedd52d9eb0
R3DM4ST34 <prafullaranjanweb@gmail.com>
2024-08-09 03:07:57 +0530

    packages updated

--- packages/@core/theme/tsup.config.ts
+++ packages/@core/theme/tsup.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig((options) => ({
-  ...options,
   name: "theme",
   entry: ["src/index.ts"],
   format: ["cjs", "esm"],
   dts: true,
   sourcemap: true,
   external: ["react", "@xtineui/base"],
+  ...options,
 }));
